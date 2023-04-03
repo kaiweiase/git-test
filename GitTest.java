8d18280bd7e65681790285864c651239367a58bb
dongkaiwei <2794139254@qq.com>
2023-04-03 10:54:08 +0800

    first commit

---
+++ GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author kaiwei
+ * @version 1.0
+ * @Date:{2023/4/3}
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
